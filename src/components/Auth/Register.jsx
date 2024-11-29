6f0f16a7fd17c46d7ccbad32fd323773cc8b3fb4
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-11-29 23:48:16 +0530

    Update Register.jsx

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8000/api/v1/user/register",
+        "https://jobbackend-1-prpp.onrender.com/api/v1/user/register",
         { name, phone, email, role, password },
         {
           headers: {
