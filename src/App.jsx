174f751ff668cad1c1c69c01be634a27d70b1b1d
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-11-29 23:45:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:8000/api/v1/user/getuser",
+          "https://jobbackend-1-prpp.onrender.com/api/v1/user/getuser",
           {
             withCredentials: true,
           }
