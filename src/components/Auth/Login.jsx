3aea0704641e465539c37cf2a560f8f800f9752d
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-11-29 23:47:39 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8000/api/v1/user/login",
+        "https://jobbackend-1-prpp.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
